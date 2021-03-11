d3f7a556784875e8eddc9e0b6ade4e3b7c40ffdd
Skylinker <skylinkertu33@gmail.com>
2021-03-11 15:36:35 +0700

    Changes files configure projects

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,37 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable quote-props */
+
 const path = require('path')
 
 module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Photocage'
+    }
+  },
+  assetsDir: 'assets',
   publicPath: '/',
+  outputDir: 'dist',
   lintOnSave: process.env.NODE_ENV !== 'production',
   runtimeCompiler: true,
   productionSourceMap: process.env.NODE_ENV !== 'production',
-
-  chainWebpack: config => {
-    config.resolve.alias.set('@', path.resolve('src'))
-    config.resolve.alias.set('@views', path.resolve('src/views/'))
-    config.resolve.alias.set('@assets', path.resolve('src/assets'))
-    config.resolve.alias.set('@router', path.resolve('src/router'))
-    config.resolve.alias.set('@components', path.resolve('src/components'))
+  css: {
+    extract: true,
+    sourceMap: true
   },
-  devServer: {
-    historyApiFallback: true,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve('src'),
+        'views': path.resolve('src/views/'),
+        'assets': path.resolve('src/assets'),
+        'router': path.resolve('src/router'),
+        'components': path.resolve('src/components'),
+      }
+    }
   }
+  // chainWebpack: config => {}
 }
