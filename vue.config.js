f162e7a60c2901f43d43d0dbb896c437bbf5fade
Skylinker <skylinkertu33@gmail.com>
2021-03-15 12:17:28 +0700

    Added (NPM-package): Package for using "less" global variables in vue-components

--- vue.config.js
+++ vue.config.js
@@ -32,5 +32,14 @@ module.exports = {
         'components': path.resolve('src/components'),
       }
     },
+  },
+
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [
+        path.join(__dirname, './src/assets/less/_var.less')
+      ]
+    }
   }
 }
