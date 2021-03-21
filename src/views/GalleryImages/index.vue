e097480cd0c337f953221d6cb443a6a5b1951187
Skylinker <skylinkertu33@gmail.com>
2021-03-21 17:34:58 +0700

    Changes (gallery) page:
    - Most of the styles have been replaced with a set of css rules from "tailwindcss"
    - Added custom css rules to the "tailwindcss" config

--- src/views/GalleryImages/index.vue
+++ src/views/GalleryImages/index.vue
@@ -1,52 +1,54 @@
 <template>
-  <section class="main__col-2 block-gallery py-4">
+  <section class="gallery w-full py-4">
     <div
       v-if="files.length"
-      class="grid grid-cols-4 gap-x-1 gap-y-2 coll-2__block-gallery gallery px-3"
+      class="gallery__grid grid grid-cols-auto-200 gap-2 px-3"
     >
       <article
         v-for="img in files"
         :key="`gallery-image-${img.id}`"
-        class="gallery-post justify-self-center align-self-center overflow-hidden"
+        class="gallery__card justify-self-center align-self-center overflow-hidden"
       >
-        <figure class="image">
+        <figure class="gallery__card__figure card-figure m-1.5">
           <a
             href=""
-            :title="img.title"
+            title="190x160.jpg"
           >
             <img
               v-if="!img.src"
               src="/assets/img/placeholder-image-190x160.jpg"
               alt="190x160.jpg"
-              class="img"
+              class="card-figure__image"
             />
             <img
               v-else
               :src="img.src"
               :alt="img.title"
-              class="img"
+              class="card-figure__image"
             />
           </a>
-          <figcaption class="title-img">
+          <figcaption class="card-figure__caption mt-1">
             <p class="truncate">
               {{ img.title }}
             </p>
           </figcaption>
         </figure>
-        <div class="text-post">
-          <p class="text-img">
+        <div class="card-figure__desc my-2">
+          <p class="text-center">
             {{ img.description }}
           </p>
         </div>
-        <hr />
-        <div class="share-icon">
-          Will be added later: Icons share social
+        <hr class="m-1.5" />
+        <div class="card-figure__socials share-icon text-center p-1">
+          <span>
+            Will be added later: Icons share social
+          </span>
         </div>
       </article>
     </div>
     <div
       v-else
-      class="coll-2__block-gallery gallery__message text-center m-0 py-5 px-1"
+      class="gallery__message text-center m-0 py-5 px-1"
     >
       <p>Ваша галлерея пуста. Вы можете загрузить вашу фото-галлерею на главной странице.</p>
     </div>
@@ -75,87 +77,73 @@
 </script>
 
 <style lang="less" scoped>
-  .gallery__message {
-    text-align: center;
-  }
-
-  .main__col-1,
-  .main__col-2 {
-    display: block;
-  }
-
-  .main__col-2 {
-    width: 100%;
-    overflow: auto;
-    overflow-x: hidden;
-  }
-
   .gallery {
-    min-height: 100%;
+    &__message {
+      text-align: center;
+    }
 
-    &-post {
-      box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);
+    &__card {
+      box-shadow: 0 0 5px 1px rgba(#000, 40%);
       width: 200px;
+
+      p {
+        color: #616060;
+        font-family: 'Roboto Regular';
+        text-align: center;
+      }
+
+      hr {
+        width: calc(100% - (2 * @tailwind-margin1-5));
+      }
+
+      &__figure {
+        display: inline-block;
+        height: 160px;
+        width: calc(100% - (2 * @tailwind-margin1-5));
+      }
 
       &:nth-of-type(4n) {
         margin-right: 0;
       }
-
-      hr {
-        width: 190px;
-        margin: 6px;
-      }
     }
   }
 
-  .image {
-    display: inline-block;
-    height: 160px;
-    margin: 5px;
-    width: 190px;
+  .card-figure {
+    img {
+      height: inherit;
+    }
 
     a {
-      width: 190px;
-      height: 160px;
+      max-height: 160px;
       display: flex;
       align-items: center;
       justify-content: center;
       margin: 0;
       padding: 0;
 
-      > .img {
+      > &__image {
         border-radius: 5px;
-        height: inherit;
       }
     }
-  }
 
-  .title-img,
-  .text-img {
-    color: #616060;
-    font-family: 'Roboto Regular';
-    text-align: center;
-  }
+    &__caption {
+      p {
+        font-size: 12pt;
+      }
+    }
 
-  .title-img {
-    font-size: 12pt;
-  }
+    &__desc {
+      p {
+        font-size: 10pt;
+      }
+    }
 
-  .text-post {
-    display: flex;
-  }
-
-  .text-img {
-    font-size: 10pt;
-    margin: 8px 0;
-    text-align: center;
-    width: 100%;
-  }
-
-  .share-icon {
-    color: #a3a2a2;
-    font-size: 9pt;
-    padding: 2px;
-    text-align: center;
+    /* TODO: Когда будут кнопки социальных сетей */
+    &__socials {
+      // span:not(class) {
+        color: #a3a2a2;
+        font-size: 9pt;
+      // }
+    }
   }
 </style>
