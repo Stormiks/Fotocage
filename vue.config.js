dfd213212f42a926a5211aa2b26950b8378f9641
Skylinker <skylinkertu33@gmail.com>
2021-03-22 21:02:22 +0700

    Added/Changes configuration:
    - Added new validate section script to rules eslint "curly"
    - Added new alias for workspace "plugins" and change vue config
    - Install loader "sass" and new npm-dependency "body-scroll-lock"

--- vue.config.js
+++ vue.config.js
@@ -31,6 +31,7 @@ module.exports = {
         'assets': path.resolve('src/assets'),
         'router': path.resolve('src/router'),
         'components': path.resolve('src/components'),
+        'plugins': path.resolve('/src/components/plugins')
       }
     },
   },
