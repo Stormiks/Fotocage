5c35f8a6c1eaadf4a98811276620a9c1d9bd3e9e
Skylinker <skylinkertu33@gmail.com>
2021-03-29 13:59:15 +0700

    Changes (progress bar):
    - The progress bar was replaced with another one, with customization and implementation of its functionality
    - Css rules refactored a bit

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import 'assets/styl/style.styl'
 import Vue from 'vue'
 import App from '@/App.vue'
 
-import VueProgressBar from 'vue-progressbar'
 import VueJSModal from 'plugins/VueJSModal/'
 import VueAxios from 'vue-axios'
 import axios from 'axios'
@@ -18,21 +17,6 @@ import { makeServer } from './server'
 /**
  * Use plugins in VueJS
  */
-const optionsProgressBar = {
-  color: '#bffaf3',
-  failedColor: '#874b4b',
-  thickness: '5px',
-  transition: {
-    speed: '0.2s',
-    opacity: '0.6s',
-    termination: 300
-  },
-  autoRevert: true,
-  location: 'left',
-  inverse: false
-}
-
-Vue.use(VueProgressBar, optionsProgressBar)
 Vue.use(VueJSModal)
 Vue.use(VueAxios, axios)
 
@@ -40,7 +24,7 @@ Vue.use(VueAxios, axios)
 // else if (process.env.NODE_ENV === 'test') makeServer('test')
 makeServer()
 
-new Vue({
+export default new Vue({
   router,
   store,
   render: h => h(App)
--- src/router/hooks.js
+++ src/router/hooks.js
@@ -1,19 +1,25 @@
 import Vue from 'vue'
 import store from '@/store/'
+import 'assets/css/vendor/nprogress.less'
+import NProgress from 'assets/js/nprogress.js'
 
 function isAuthUser(route, status, next) {
-  if (status)
+  if (status) {
     if (route === 'Login' || route === 'Registation')
       next({ name: 'Home' })
     else next()
-  else
-  if (route === 'Login' || route === 'Registation')
-    next()
-  else next({ name: 'Login' })
+    NProgress.inc(0.5)
+  } else {
+    if (route === 'Login' || route === 'Registation')
+      next()
+    else next({ name: 'Login' })
+    NProgress.inc(0.5)
+  }
 }
 
 // eslint-disable-next-line no-multiple-empty-lines
-export const before = (to, from, next) => {
+export const before = function (to, from, next) {
+  NProgress.set(0.0)
   if (store.state.isLogin)
     store.dispatch('getAuthStatusByServer').then(status => {
       isAuthUser(to.name, status, next)
@@ -25,5 +31,6 @@ export const after = (to, from) => {
   // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
   Vue.nextTick(() => {
     document.title = to.meta.title || ''
+    NProgress.set(1.0)
   })
 }
