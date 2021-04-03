e1f9217aa3e83bd1d24a4259ef3ee786f7310388
Skylinker <skylinkertu33@gmail.com>
2021-04-03 16:44:02 +0700

    Change (login page): Added login form validation using "vuevalidate" library

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,46 +1,78 @@
 <template>
   <section class="auth-login">
     <AuthFormLayout
-      :authSubmit="login"
+      @auth-submit="login(form)"
+      :error-form="!$v.validForm.$invalid"
       class="form__auth_login"
     >
       <h1>Чтобы добавлять свои фотографии, войдите!</h1>
       <AuthFormGroup class="flex flex-col mb-4">
-        <div class="form__input_box mb-2.5 relative">
-          <div class="form__input_icon flex items-center px-2 absolute">
-            <SvgIcon
-              :name="`login-user`"
+        <div class="form__input_box mb-2.5">
+          <div class="relative">
+            <div class="form__input_icon flex items-center px-2 absolute">
+              <SvgIcon
+                :name="`login-user`"
+              />
+            </div>
+            <input
+              class="w-full border"
+              :class="{ 'is-invalid': $v.form.login.$error }"
+              v-model.trim="form.login"
+              id="username"
+              name="name"
+              type="text"
+              placeholder="Логин"
+              autofocus
             />
           </div>
-
-          <input
-            class="w-full"
-            v-model="user.login"
-            id="username"
-            name="name"
-            type="text"
-            placeholder="Логин"
-            autofocus />
+          <span
+            class="px-1 py-0.5 text-red-600"
+            v-if="!$v.form.login.required && $v.form.login.$dirty"
+          >
+            Введите логин
+          </span>
+          <span
+            class="px-1 py-0.5 text-red-600"
+            v-else-if="!$v.form.login.minLength && $v.form.login.$dirty"
+          >
+            Минимальная длина логина {{ $v.form.login.$params.minLength.min }}
+          </span>
         </div>
-        <div class="form__input_box relative">
-          <div class="form__input_icon flex items-center px-2 absolute">
-            <SvgIcon
-              :name="`login-key`"
+        <div class="form__input_box">
+          <div class="relative">
+            <div class="form__input_icon flex items-center px-2 absolute">
+              <SvgIcon
+                :name="`login-key`"
+              />
+            </div>
+
+            <input
+              class="w-full border"
+              :class="{ 'is-invalid': $v.form.password.$error }"
+              v-model.trim="form.password"
+              id="password"
+              name="pass"
+              type="password"
+              placeholder="Пароль"
             />
           </div>
-
-          <input
-            class="w-full"
-            v-model="user.password"
-            id="password"
-            name="pass"
-            type="password"
-            placeholder="Пароль" />
+          <span
+            class="px-1 py-0.5 text-red-600"
+            v-if="!$v.form.password.required && $v.form.password.$dirty"
+          >
+            Введите пароль
+          </span>
+          <span
+            class="px-1 py-0.5 text-red-600"
+            v-else-if="!$v.form.password.minLength && $v.form.password.$dirty"
+          >
+            Минимальная длина пароля {{ $v.form.password.$params.minLength.min }}
+          </span>
         </div>
       </AuthFormGroup>
       <AuthFormFooter>
         <input
-          :disabled="!validForm"
+          :disabled="$v.validForm.$invalid"
           type="submit"
           name="val"
           value="ВОЙТИ" />
@@ -55,6 +87,8 @@
 </template>
 
 <script>
+  import { validationMixin } from 'vuelidate'
+  import { required, minLength } from 'vuelidate/lib/validators'
   import { mapState } from 'vuex'
   import AuthFormLayout from 'layoutAuth/components/AuthFormLayout'
   import AuthFormGroup from 'layoutAuth/components/AuthFormGroup'
@@ -70,47 +104,39 @@
     },
     name: 'ViewsLogin',
     data: () => ({
-      user: {
+      form: {
         login: '',
         password: ''
-      },
-      validLogin: false,
-      validPassword: false
+      }
     }),
-    watch: {
-      'user.login': {
-        immediate: true,
-        handler: function (newVal) {
-          if (newVal !== '' && newVal !== null)
-            this.validLogin = true
-          else this.validLogin = false
-        }
-      },
-      'user.password': {
-        immediate: true,
-        handler: function (newVal) {
-          if (newVal !== '' && newVal !== null && newVal.length >= 4)
-            this.validPassword = true
-          else this.validPassword = false
+    validations: {
+      validForm: ['form.login', 'form.password'],
+      form: {
+        login: {
+          required,
+          minLength: minLength(3)
+        },
+        password: {
+          required,
+          minLength: minLength(4)
         }
       }
     },
     computed: {
       ...mapState({
         logged: state => state.isLoggedIn
-      }),
-      validForm() {
-        return this.validLogin === true && this.validPassword === true
-      }
+      })
     },
     beforeRouteLeave(to, from, next) {
       if (to.name !== 'Registration') next(this.logged)
       else next()
     },
     methods: {
-      login() {
-        if (this.validForm)
-          this.axios.post('/api/login', { ...this.user }).then(res => {
+      login(formData) {
+        this.$v.form.$touch()
+
+        if (!this.$v.validForm.$error)
+          this.axios.post('/api/login', { ...formData }).then(res => {
             if (res.data.status) {
               this.$store.dispatch('updateStatusLogin', res.data.user)
 
@@ -118,7 +144,8 @@
             } else { this.$store.dispatch('updateStatusLogin', { auth: false }) }
           }).catch(e => console.log(e))
       }
-    }
+    },
+    mixins: [validationMixin]
   }
 </script>
 
@@ -147,6 +174,10 @@
         border: 1px solid #ccc;
         border-radius: 5px;
         box-shadow: 0 1px 1px #ccc inset, 0 1px 0 @color-white;
+
+        &.is-invalid {
+          border-color: #ef4444;
+        }
 
         &:focus {
           background-color: @color-white;
