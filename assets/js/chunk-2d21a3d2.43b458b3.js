864b9f352b2a36ca8a2f933bafe09d8b5a09b9a7
Skylinker <skylinkertu33@gmail.com>
2021-04-16 10:59:25 +0700

    Build release-1.0.0

---
+++ assets/js/chunk-2d21a3d2.43b458b3.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21a3d2"],{bb51:function(e,n,t){"use strict";t.r(n);var s=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("section",{staticClass:"home"},[t("router-view")],1)},a=[],c={name:"ViewsHome"},o=c,u=t("2877"),i=Object(u["a"])(o,s,a,!1,null,null,null);n["default"]=i.exports}}]);
